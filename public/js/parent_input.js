be346f229efec2bd20563c9044bc5e57123feb09
Yvette <yvettetran@gmail.com>
2018-03-08 00:26:41 -0800

    all students show for parent view class profile, need to figure out how to show only the parent's students

--- public/js/parent_input.js
+++ public/js/parent_input.js
@@ -43,60 +43,17 @@ $(document).ready(function () {
           location.reload();
         }
       );
-      // $.post("/api/parent/add", newStudent, function() {
-      //   console.log("student posted!");
-      //   location.reload();
-      // });
   });
-
-
-  // var firstnameInput = $("#firstname-input").val().trim();
-  // var lastnameInput = $("#lastname-input").val().trim();
-  // var birthdateInput = $("#birthdate-input").val().trim();
-  // var parentIdInput = $("#parent-ID").val().trim();
-  // var staffIdInput = $("#staff-ID").val().trim();
-  // var asthma = $("[name='asthma_radio']:checked").val();
-  // var studentAllergy = $("#student-allergies").val().trim();
-  // var epiPen = $("[name='epiPen_radio']:checked").val();
-  // var chronicCon = $("#chronic-condition").val().trim();
-
-  // function StudentFormSubmit(event) {
-  //   event.preventDefault();
-  //   // Don't do anything if the name fields hasn't been filled out
-  //   if (!formInputs.val().trim().trim() || !formCheckInputs.val().trim().trim()) {
-  //     return;
-  //   }
-  //   // if (!firstnameInput.val().trim().trim() || !lastnameInput.val().trim().trim() || !birthdateInput.val().trim().trim()) {
-  //   //   return;
-  //   // }
-  //   else {
-  //     var newStudent = {
-  //       first_name: firstnameInput,
-  //       last_name: lastnameInput,
-  //       birth_date: birthdateInput,
-  //       grade: studentGrade,
-  //       parent_id: parentIdInput,
-  //       teacher_id: staffIdInput,
-  //       asthma: asthma,
-  //       allergy: studentAllergy,
-  //       epi_pen: epipen,
-  //       chronic_condition: chronicCon
-  //     };
-
-  //     $.ajax("/api/parent/add", {
-  //       type: "POST",
-  //       data: newStudent
-  //     }).then(function() {
-  //         console.log("created new cat");
-  //         // Reload the page to get the updated list
-  //         location.reload();
+ 
+  // View all students pertaining to a parent
+  // function viewClassProfile(){
+  //   $.ajax("/api/parent/class", {
+  //       type: "GET"
+  //     }).then(function(data) {
+          
   //       }
   //     );
-  //   }
   // }
-
- 
-
     // Adding event listeners to initiate AJAX call to get the allergies, the presence of asthmatics, and other chronic conditions
   // $(document).on("click", "#", getClass);
 
--- routes/parent-api-routes.js
+++ routes/parent-api-routes.js
@@ -5,24 +5,23 @@ module.exports = function(app) {
   // this GET grabs info for the parent screen(..right?)
   // –– attributes will be the columns whose values
   //    we want to be able to disply to the user
-  // app.get("/api/parent/class", function(req, res) {
+  app.get("/api/parent/class", function(req, res) {
     
-  //   // this would display to the parent user their own info
-  //   db.Parent.findAll({
-  //     where: {parent_id: req.body.id},
-  //     include: [{
-  //       model: db.Student,
-  //       through: {
-  //         attributes: [
-  //           // any student values we'd want would go here
-  //         ],
-  //       }
-  //     }]
-  //   }).then(function(dbStudent) {
-  //     console.log(dbStudent);
-  //     res.json(dbStudent);
-  //   });
-
+    // this would display to the parent user their own info
+    db.Student.findAll({
+      // where: {parent_id: req.body.id},
+      // include: [{
+      //   model: db.Student,
+      //   through: {
+      //     attributes: [
+      //       // any student values we'd want would go here
+      //     ],
+      //   }
+      // }]
+    }).then(function(data) {
+      res.render("class", {class: data});
+    });
+  });
   //   // =========================
   //   // this grabs data for the parent's student(s),
   //   // AND includes that student's teacher's name,
