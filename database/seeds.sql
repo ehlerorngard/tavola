3e54bda83d7dd25367b4707af190e21323c68bee
Yvette <yvettetran@gmail.com>
2018-03-08 11:31:05 -0800

    login/password.js intergrated

--- database/seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO parents (first_name, last_name, phone_number, parentMedConsent)
-VALUES ("Frank", "Ocean", 408, true), ("Jane", "Doe", 000, false);
-
-INSERT INTO staffs (first_name, last_name, phone_number, email)
-VALUES ("Johnny", "Bravo", 555, "jbravo@school.edu"), ("Patty", "Cake", 341, "pcake@school.edu");
-
-INSERT INTO students (first_name, last_name, birth_date, allergy, chronic_condition)
-VALUES ("Jon", "Snow", "12/05/1995", "dairy", "adjusting to parent divorce");
