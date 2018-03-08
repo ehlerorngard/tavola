7b33857e8aed4bfa62fdcd833fa9252b2586b00c
Brandan Richardson <brandanr@gmail.com>
2018-03-08 13:39:58 -0800

    seeds.sql up for the user, staff, and parent models

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,8 +1,29 @@
-INSERT INTO parents (first_name, last_name, phone_number, parentMedConsent)
-VALUES ("Frank", "Ocean", 408, true), ("Jane", "Doe", 000, false);
+INSERT INTO parent (first_name, last_name, phone_number, parentMedConsent)
+VALUES ("Kobe", "Bryant", 4087659877, true), 
+("LeBron", "James", 6509234561, false),
+("Steph", "Curry", 3109877885, false),
+("Kevin", "Durant", 4154439928, true),
+("Kevin", "Hart", 7652109887, false),
+("Draymond", "Green", 6895432167, false),
+("Klay", "Thompson", 8285432098, true),
+("Lonzo", "Ball", 2129076342, true),
+("Serena", "Williams", 6198777390, false),
+("Kate", "Middleton", 7279017543, false);
 
-INSERT INTO staffs (first_name, last_name, phone_number, email)
-VALUES ("Johnny", "Bravo", 555, "jbravo@school.edu"), ("Patty", "Cake", 341, "pcake@school.edu");
+INSERT INTO staff (first_name, last_name, phone_number, email, username)
+VALUES ("Johnny", "Bravo", 5555763427, "jbravo@school.edu", "jbravo"), 
+("Patty", "Cake", 3411433414, "pcake@school.edu", "pcake"), 
+("Rocky", "Balboa", 8085432789, "stallion@school.edu", "stallion"),
+("Tony", "Stark", 4145647889, "ironman@school.edu", "ironman"),
+("Lavar", "Ball", 9095443217, "bbb@school.edu", "bbb"),
+("Tom", "Brady", 3237564326, "goat@school.edu", "goat");
 
-INSERT INTO students (first_name, last_name, birth_date, allergy, chronic_condition)
-VALUES ("Jon", "Snow", "12/05/1995", "dairy", "adjusting to parent divorce");
+INSERT INTO user (username, password)
+VALUES ( "jbravo","12345"), 
+("pcake", "12345"), 
+("stallion", "12345"),
+("ironman", "12345"),
+("bbb", "12345"),
+("goat", "12345");
+
+
